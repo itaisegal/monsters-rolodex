7cd0f67d0385c2097ffcfe414b110d873c1745ce
itai segal <itai.segal@gmail.com>
2020-01-06 15:21:59 +0200

    Updates

---
+++ precache-manifest.436d83f6e54cf6969fd69b21ed527107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5d1cdb5e50f47e9084c0f328590e81",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5d6e07c7db4b4fab358",
+    "url": "/monsters-rolodex/static/css/main.d6d9577f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5d6e07c7db4b4fab358",
+    "url": "/monsters-rolodex/static/js/main.b5c0b255.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
